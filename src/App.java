9647e6c40be4f1286a22368bbbd088355c9df560
Sidharth Shukla <swayam1044@gmail.com>
2023-02-11 23:59:18 +0530

    welcome

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!!");
         System.out.println("hii i m sidharth");
+        System.out.println("welcome");
     }
 }
