b7938c2e94ce6b5ea6bc523ff3b9611a2d3f5804
Sidharth Shukla <swayam1044@gmail.com>
2023-02-13 01:39:07 +0530

    minimum in array

---
+++ src/minimumNoinarray.java
@@ -0,0 +1,16 @@
+
+public class minimumNoinarray
+{
+  public static void main(String args[])
+  {
+    int[] arr= {5,3,2,4,65};
+     int min = arr[0];
+
+  for (int i = 0; i < arr.length; i++) {
+    if(min>arr[i]){
+        min = arr[i];
+    }
+  }
+  System.out.println(min + " ");
+  }
+}
