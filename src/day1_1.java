33a4587541b5bf989c028b5a29fa2328b51f9e4d
Sidharth Shukla <swayam1044@gmail.com>
2023-02-12 16:00:53 +0530

    mutability of array in java

---
+++ src/day1_1.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class day1_1 {
+    public static void main(String[] args) {
+        int[] nums  = {3,4,5,6};
+        System.out.println(Arrays.toString(nums));
+        change(nums);
+        System.out.println(Arrays.toString(nums));// arrays in java are mutable....we can change the object in java
+    }
+
+    static void change(int[] arr){
+         arr[0]= 99;
+    }
+}
