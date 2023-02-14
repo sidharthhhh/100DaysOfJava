6509a656df0bce455981092d7ab09a7a8be371d0
Sidharth Shukla <swayam1044@gmail.com>
2023-02-15 02:49:06 +0530

    perfect square day3

---
+++ src/perfectSquare.java
@@ -0,0 +1,18 @@
+public class perfectSquare {
+
+    static boolean isperfectsquare(int x){
+        if(x>=0){
+            int sq = (int)Math.sqrt(x);
+            return ((sq*sq) == x);
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int x = 34;
+        if(isperfectsquare(x)){
+            System.out.println(true);
+        }else{
+            System.out.println(false);
+        }
+    }
+}
