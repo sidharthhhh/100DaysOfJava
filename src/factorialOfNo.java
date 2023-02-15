e9ef5980588159c9ba5be56ad01afe13f5201c73
Sidharth Shukla <swayam1044@gmail.com>
2023-02-16 01:16:38 +0530

    factorial of number day4

---
+++ src/factorialOfNo.java
@@ -0,0 +1,14 @@
+public class factorialOfNo {
+
+    public static int factorial(int n){
+     int res= 1;
+     for (int i = 2; i <= n; i++) {
+        res = res*i;
+     }
+     return res;
+    }
+    public static void main(String[] args) {
+        int num = 6;
+        System.out.println("factorial of " + num + " is a " + factorial(num));
+    }
+}
